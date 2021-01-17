da78c50fd441154c535256f2f927ce523af61e28
Aaron McGuire <samuel.aaron.mcguire@gmail.com>
2021-01-16 22:25:40 -0800

    deploy

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,22 @@
-import path from 'path';
-import resolve from '@rollup/plugin-node-resolve';
-import replace from '@rollup/plugin-replace';
-import commonjs from '@rollup/plugin-commonjs';
-import url from '@rollup/plugin-url';
-import svelte from 'rollup-plugin-svelte';
-import babel from '@rollup/plugin-babel';
-import { terser } from 'rollup-plugin-terser';
-import config from 'sapper/config/rollup.js';
-import pkg from './package.json';
+import path from "path";
+import resolve from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
+import commonjs from "@rollup/plugin-commonjs";
+import url from "@rollup/plugin-url";
+import svelte from "rollup-plugin-svelte";
+import babel from "@rollup/plugin-babel";
+import { terser } from "rollup-plugin-terser";
+import config from "sapper/config/rollup.js";
+import pkg from "./package.json";
 
 const mode = process.env.NODE_ENV;
-const dev = mode === 'development';
+const dev = mode === "development";
 const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 
 const onwarn = (warning, onwarn) =>
-	(warning.code === 'MISSING_EXPORT' && /'preload'/.test(warning.message)) ||
-	(warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) ||
+	(warning.code === "MISSING_EXPORT" && /'preload'/.test(warning.message)) ||
+	(warning.code === "CIRCULAR_DEPENDENCY" &&
+		/[/\\]@sapper[/\\]/.test(warning.message)) ||
 	onwarn(warning);
 
 export default {
@@ -24,45 +25,53 @@ export default {
 		output: config.client.output(),
 		plugins: [
 			replace({
-				'process.browser': true,
-				'process.env.NODE_ENV': JSON.stringify(mode)
+				"process.browser": true,
+				"process.env.NODE_ENV": JSON.stringify(mode),
 			}),
 			svelte({
 				compilerOptions: {
 					dev,
-					hydratable: true
-				}
+					hydratable: true,
+				},
 			}),
 			url({
-				sourceDir: path.resolve(__dirname, 'src/node_modules/images'),
-				publicPath: '/client/'
+				sourceDir: path.resolve(__dirname, "src/node_modules/images"),
+				publicPath: "/client/",
 			}),
 			resolve({
 				browser: true,
-				dedupe: ['svelte']
+				dedupe: ["svelte"],
 			}),
 			commonjs(),
 
-			legacy && babel({
-				extensions: ['.js', '.mjs', '.html', '.svelte'],
-				babelHelpers: 'runtime',
-				exclude: ['node_modules/@babel/**'],
-				presets: [
-					['@babel/preset-env', {
-						targets: '> 0.25%, not dead'
-					}]
-				],
-				plugins: [
-					'@babel/plugin-syntax-dynamic-import',
-					['@babel/plugin-transform-runtime', {
-						useESModules: true
-					}]
-				]
-			}),
+			legacy &&
+				babel({
+					extensions: [".js", ".mjs", ".html", ".svelte"],
+					babelHelpers: "runtime",
+					exclude: ["node_modules/@babel/**"],
+					presets: [
+						[
+							"@babel/preset-env",
+							{
+								targets: "> 0.25%, not dead",
+							},
+						],
+					],
+					plugins: [
+						"@babel/plugin-syntax-dynamic-import",
+						[
+							"@babel/plugin-transform-runtime",
+							{
+								useESModules: true,
+							},
+						],
+					],
+				}),
 
-			!dev && terser({
-				module: true
-			})
+			!dev &&
+				terser({
+					module: true,
+				}),
 		],
 
 		preserveEntrySignatures: false,
@@ -74,30 +83,32 @@ export default {
 		output: config.server.output(),
 		plugins: [
 			replace({
-				'process.browser': false,
-				'process.env.NODE_ENV': JSON.stringify(mode)
+				"process.browser": false,
+				"process.env.NODE_ENV": JSON.stringify(mode),
 			}),
 			svelte({
 				compilerOptions: {
 					dev,
-					generate: 'ssr',
-					hydratable: true
+					generate: "ssr",
+					hydratable: true,
 				},
-				emitCss: false
+				emitCss: false,
 			}),
 			url({
-				sourceDir: path.resolve(__dirname, 'src/node_modules/images'),
-				publicPath: '/client/',
-				emitFiles: false // already emitted by client build
+				sourceDir: path.resolve(__dirname, "src/node_modules/images"),
+				publicPath: "/client/",
+				emitFiles: false, // already emitted by client build
 			}),
 			resolve({
-				dedupe: ['svelte']
+				dedupe: ["svelte"],
 			}),
-			commonjs()
+			commonjs(),
 		],
-		external: Object.keys(pkg.dependencies).concat(require('module').builtinModules),
+		external: Object.keys(pkg.dependencies).concat(
+			require("module").builtinModules
+		),
 
-		preserveEntrySignatures: 'strict',
+		preserveEntrySignatures: "strict",
 		onwarn,
 	},
 
@@ -107,14 +118,14 @@ export default {
 		plugins: [
 			resolve(),
 			replace({
-				'process.browser': true,
-				'process.env.NODE_ENV': JSON.stringify(mode)
+				"process.browser": true,
+				"process.env.NODE_ENV": JSON.stringify(mode),
 			}),
 			commonjs(),
-			!dev && terser()
+			!dev && terser(),
 		],
 
 		preserveEntrySignatures: false,
 		onwarn,
-	}
+	},
 };
