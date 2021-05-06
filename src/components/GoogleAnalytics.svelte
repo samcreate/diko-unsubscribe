687c48f665e946b3dfd20115a002157924a14a5b
Aaron McGuire <samuel.aaron.mcguire@gmail.com>
2021-05-06 10:22:43 -0700

    ga

---
+++ src/components/GoogleAnalytics.svelte
@@ -0,0 +1,14 @@
+<script>
+	// https://tute.io/google-analytics-svelte-sapper
+	import { stores } from "@sapper/app";
+	const { page, session } = stores();
+
+	$: {
+		/* eslint-disable no-undef */
+		if (typeof gtag !== "undefined") {
+			gtag("config", "G-TEDZ0RF35G", {
+				page_path: $page.path,
+			});
+		}
+	}
+</script>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,23 +1,25 @@
 <script>
-  import Nav from "../components/Nav.svelte";
+	import Nav from "../components/Nav.svelte";
+	import GoogleAnalytics from "../components/GoogleAnalytics.svelte";
 
-  export let segment;
+	export let segment;
 </script>
 
-<style>
-  main {
-    width: 100vw;
-    height: 100vh;
-  }
-  @media only screen and (max-width: 39.1875rem) {
-    main {
-      width: 100%;
-      height: 100%;
-      min-height: -webkit-fill-available;
-    }
-  }
-</style>
-
 <main>
-  <slot />
+	<slot />
 </main>
+<GoogleAnalytics />
+
+<style>
+	main {
+		width: 100vw;
+		height: 100vh;
+	}
+	@media only screen and (max-width: 39.1875rem) {
+		main {
+			width: 100%;
+			height: 100%;
+			min-height: -webkit-fill-available;
+		}
+	}
+</style>
